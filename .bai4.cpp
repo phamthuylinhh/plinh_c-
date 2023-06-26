2f6b88213e52bf63e62dd38bc3557b886953cab8
phamthuylinhh <115873781+phamthuylinhh@users.noreply.github.com>
2023-06-26 07:55:00 +0700

    add file Tuan3

---
+++ .bai4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+	string ham ( int n) {
+		if(n>=1 && n<=9)
+		{
+			string number[]= {"one ","two","three","four","five","six","seven","eight","night", "even","odd"};
+			return number[n-1];
+		}else if(n>9) {
+			if(n%2== 0){
+			return "even";
+		}else {
+			return "odd";	}		
+	}
+}
+	int main() {
+		int a;
+		int b;
+	cin>>a>>b;
+	if(a<=b && a>=1 && b<=9){
+		for( int i = a; i<=b; i++){
+			cout<<i <<std::endl;
+			}
+	cout<<a<<std::endl;
+	cout<<b<<std::endl;;
+	return 0;		
+	}
+};
+
+
---
+++ 2.4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x = 200000;
+	int y = 1000;
+	int days = 30;
+	int S = 0;
+	for (int i = 1; i<=30; i++){
+		S= S+y;
+		y = y-1000;
+	}
+	 S = S+x;
+	 cout<<S<<" la so tien sau 1 thang ";
+	 return 0;
+}
