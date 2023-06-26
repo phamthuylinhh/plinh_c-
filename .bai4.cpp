66155f0acfe2e8fa752126dbfc89cad09ab7271c
phamthuylinhh <115873781+phamthuylinhh@users.noreply.github.com>
2023-06-26 18:36:49 +0700

    add file Tuan3

--- .bai4.cpp
+++ .bai4.cpp
@@ -6,23 +6,27 @@ using namespace std;
 		{
 			string number[]= {"one ","two","three","four","five","six","seven","eight","night", "even","odd"};
 			return number[n-1];
-		}else if(n>9) {
-			if(n%2== 0){
-			return "even";
-		}else {
-			return "odd";	}		
-	}
+		} else if (n > 9)
+			{
+			if( n % 2 == 0) {
+				return "even";
+		} else {
+		  		return "odd";
+		  	}
+		return "";
+		  		
 }
-	int main() {
-		int a;
-		int b;
+int main() 
+{
+	int a;
+	int b;
 	cin>>a>>b;
-	if(a<=b && a>=1 && b<=9){
-		for( int i = a; i<=b; i++){
-			cout<<i <<std::endl;
-			}
-	cout<<a<<std::endl;
-	cout<<b<<std::endl;;
+		if(a<=b && a>=1 && b<=9)
+	{
+		for( int i = a; i<=b; i++)
+			{cout<<i <<std::endl;}
+		cout<<a<<std::endl;
+		cout<<b<<std::endl;;
 	return 0;		
 	}
 };
--- 2.4.cpp
+++ 2.4.cpp
@@ -7,7 +7,7 @@ int main(){
 	int S = 0;
 	for (int i = 1; i<=30; i++){
 		S= S+y;
-		y = y-1000;
+		y = y+1000;
 	}
 	 S = S+x;
 	 cout<<S<<" la so tien sau 1 thang ";
